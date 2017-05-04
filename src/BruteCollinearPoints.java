914f07cc4a5336114862777b5c4a24e099472dcf
leoschulmann <lev.chigintsev@gmail.com>
2017-05-04 12:47:48 +0300

    added exceptions, test for equal points

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -5,25 +5,47 @@
 // between p and s are all equal.
 
 
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
 
-    private Point[] pts;
     private int numberOfSegments;
+    private LineSegment[] results = new LineSegment[10];
+    private void extendResults(){
+        LineSegment[] newR = new LineSegment[results.length*2];
+        System.arraycopy(results, 0, newR, 0, results.length);
+        results = newR;
+    }
 
-    // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points){
-        pts = points;
-        for (int p1 = 0; p1< pts.length; p1++){
-            for (int p2 = 1; p2< pts.length; p2++){
-                double refSlope = pts[p1].slopeTo(pts[p2]);
+        if (points.length == 0) throw new NullPointerException("array zero length");
+        Arrays.sort(points);
+            for (int i = 0; i < points.length; i++){
+                if (points[i] == null) throw new NullPointerException("array contains null element");
+                if (i > 0 && points[i].slopeTo(points[i-1])==Double.NEGATIVE_INFINITY) throw new IllegalArgumentException("array contains duplicate points!");
+        }
 
-                for (int p3 = 2; p3< pts.length; p3++){
-                    if (pts[p2].slopeTo(pts[p3]) != refSlope) continue; //if 2-3 slope differs from 1-2 - iterate p3
+        for (int p1 = 0; p1< points.length; p1++){
+            for (int p2 = 1; p2< points.length; p2++){
+                if (points[p2] == points[p1]) continue;
+                double refSlope = points[p1].slopeTo(points[p2]);
 
-                    for (int p4 = 3; p4< pts.length; p4++){
+                for (int p3 = 2; p3< points.length; p3++){
+                    if (points[p3] == points[p2] || points[p3] == points[p1]) continue;
+                    if (points[p2].slopeTo(points[p3]) != refSlope) continue; //if 2-3 slope differs from 1-2 - iterate p3
 
-
-
+                    for (int p4 = 3; p4< points.length; p4++){
+                        if (points[p4] == points[p3] || points[p4] == points[p2] || points[p4] == points[p1]) continue;
+                        if  (points[p3].slopeTo(points[p4]) != refSlope) continue;
+                        Point[] candidate = new Point[4];
+                        candidate[0] = points[p1];
+                        candidate[1] = points[p2];
+                        candidate[2] = points[p3];
+                        candidate[3] = points[p4];
+                        Arrays.sort(candidate);
+                        if (numberOfSegments() == results.length) extendResults();
+                        results[numberOfSegments()] = new LineSegment(candidate[0], candidate[3]);
+                        numberOfSegments++;
                     }
                 }
             }
@@ -37,6 +59,14 @@ public class BruteCollinearPoints {
         return numberOfSegments;
     }
 
+
     // the line segments
-    public LineSegment[] segments()
+    //The method segments() should include each line segment containing 4 points exactly once.
+    // If 4 points appear on a line segment in the order p→q→r→s, then you should include
+    // either the line segment p→s or s→p (but not both) and you should not include subsegments
+    // such as p→r or q→r. For simplicity, we will not supply any input to BruteCollinearPoints
+    // that has 5 or more collinear points.
+    public LineSegment[] segments(){
+        return results;
+    }
 }
